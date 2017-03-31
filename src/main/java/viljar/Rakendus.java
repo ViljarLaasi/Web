9fb9740d843b035f1ca82530126eb2f9fb3695a2
ViljarLaasi <viljarlaasi@gmail.com>
2017-03-31 13:10:53 +0300

    tes18

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -23,16 +23,16 @@ public class Rakendus {
 				int c2=Integer.parseInt(arv8);
 				int c3=Integer.parseInt(arv9);
 				return "Vektorkorrutis"+ (b2*c3-b3*c2)+" i"+(b3*c1-b1*c3)+" k"+(b1*c2-b2*c1);
-			}else(){
-			int a1=Integer.parseInt(arv1);
-			int a2=Integer.parseInt(arv2);
-			int a3=Integer.parseInt(arv3);
-			int b1=Integer.parseInt(arv4);
-			int b2=Integer.parseInt(arv5);
-			int b3=Integer.parseInt(arv6);
-			int c1=Integer.parseInt(arv7);
-			int c2=Integer.parseInt(arv8);
-			int c3=Integer.parseInt(arv9);
+			}else{
+				//	int a1=Integer.parseInt(arv1);
+				//	int a2=Integer.parseInt(arv2);
+				//	int a3=Integer.parseInt(arv3);
+				//	int b1=Integer.parseInt(arv4);
+		//	int b2=Integer.parseInt(arv5);
+		//	int b3=Integer.parseInt(arv6);
+		//	int c1=Integer.parseInt(arv7);
+		//	int c2=Integer.parseInt(arv8);
+		//	int c3=Integer.parseInt(arv9);
 
 
 			return "Determinat "+ (a1*b2*c3+a2*b3*c1+a3*b1*c2-a3*b2*c1-a1*b3*c2-a2*b1*c3);
