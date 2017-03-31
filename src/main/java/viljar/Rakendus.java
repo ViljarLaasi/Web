bbed1e7f1d35ce43d3030207891c5c7dcbff055f
ViljarLaasi <viljarlaasi@gmail.com>
2017-03-31 09:48:11 +0300

    test20

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -18,14 +18,7 @@ public class Rakendus {
 			int a2=Integer.parseInt(arv2);
 			return arv1+"ja"+arv2+"geomeetriline keskmine on " +a1*a2;
 		}
-	@RequestMapping("/tervita")
-				String gk(String arv1, String arv2){
-					if(arv1==null){return "andmed puuduvad";}
-					int a1=Integer.parseInt(arv1);
-					int a2=Integer.parseInt(arv2);
-					return arv1+"ja"+arv2+"geomeetriline keskmine on " +a1*a2;
-	    }
-
+	
 
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 35878);
