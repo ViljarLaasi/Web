dbd84d2b2a3374118e40d287c8134996a374c5e1
ViljarLaasi <viljarlaasi@gmail.com>
2017-03-31 10:15:03 +0300

    test24

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -16,7 +16,7 @@ public class Rakendus {
 			if(arv1==null){return "andmed puuduvad";}
 			int a1=Integer.parseInt(arv1);
 			int a2=Integer.parseInt(arv2);
-			return arv1+"ja "+arv2+" geomeetriline keskmine on " + Math.sqrt(Math.pow(a1,2)*Math.pow(a2,2);
+			return arv1+"ja "+arv2+" geomeetriline keskmine on " + Math.sqrt(Math.pow(a1,2)*Math.pow(a2,2));
 		}
 
 
