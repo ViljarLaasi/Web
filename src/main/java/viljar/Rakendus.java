ec94f09d5ba267eed468ff6cc65d10dd8e3a3023
ViljarLaasi <viljarlaasi@gmail.com>
2017-04-01 13:22:39 +0300

    t14

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -12,18 +12,6 @@ public class Rakendus {
     }
 	@RequestMapping("/determinant")
 		String gk(String arv1, String arv2, String arv3, String arv4, String arv5, String arv6, String arv7, String arv8, String arv9){
-			if(arv1==null){
-				//int a1=Integer.parseInt(arv1);
-				//int a2=Integer.parseInt(arv2);
-				//int a3=Integer.parseInt(arv3);
-				int b1=Integer.parseInt(arv4);
-				int b2=Integer.parseInt(arv5);
-				int b3=Integer.parseInt(arv6);
-				int c1=Integer.parseInt(arv7);
-				int c2=Integer.parseInt(arv8);
-				int c3=Integer.parseInt(arv9);
-				return "Vektorkorrutis i"+ (b2*c3-b3*c2)+" j"+(b3*c1-b1*c3)+" k"+(b1*c2-b2*c1);
-			}
 				int a1=Integer.parseInt(arv1);
 				int a2=Integer.parseInt(arv2);
 				int a3=Integer.parseInt(arv3);
@@ -33,9 +21,11 @@ public class Rakendus {
 				int c1=Integer.parseInt(arv7);
 				int c2=Integer.parseInt(arv8);
 				int c3=Integer.parseInt(arv9);
-				return "Determinat "+ (a1*b2*c3+a2*b3*c1+a3*b1*c2-a3*b2*c1-a1*b3*c2-a2*b1*c3);
-		
-
+			if(arv1==null){
+				return "Vektorkorrutis i"+ (b2*c3-b3*c2)+" j"+(b3*c1-b1*c3)+" k"+(b1*c2-b2*c1);
+			}else{
+				return "Determinant "+ (a1*b2*c3+a2*b3*c1+a3*b1*c2-a3*b2*c1-a1*b3*c2-a2*b1*c3);
+			}
 }
 
     public static void main(String[] args) {
