bc1bd0f5f1431bb91f284d4c329f6edfc93d4e20
ViljarLaasi <viljarlaasi@gmail.com>
2017-04-03 18:49:51 +0300

    1

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -19,8 +19,8 @@ public class Rakendus {
 				int c1=Integer.parseInt(arv7);
 				int c2=Integer.parseInt(arv8);
 				int c3=Integer.parseInt(arv9);
-			if(arv1=="x"){
-				return "Vektorkorrutis "+ (b2*c3-b3*c2)+"j"+(b3*c1-b1*c3)+"k"+(b1*c2-b2*c1);
+			if(arv1==" "){
+				return "Vektorkorrutis i"+ (b2*c3-b3*c2)+"j"+(b3*c1-b1*c3)+"k"+(b1*c2-b2*c1);
 			} else {
 				int a1=Integer.parseInt(arv1);
 				int a2=Integer.parseInt(arv2);
