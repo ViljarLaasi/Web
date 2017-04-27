c7bb20a011307f691ae9afd1790371bf2a1ad43b
ViljarLaasi <viljarlaasi@gmail.com>
2017-04-27 16:45:12 +0300

    tr

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -1,4 +1,4 @@
-package jaagup;
+package viljar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
